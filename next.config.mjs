be1d53d02ef2c1e032df0f0b9baf1c7d14814aae
votre_nom_github <votre_email_github>
2025-06-20 22:16:01 +0100

    premier a jour

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,23 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+     
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
